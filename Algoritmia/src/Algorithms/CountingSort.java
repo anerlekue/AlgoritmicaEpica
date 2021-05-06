c50fce63584380cdf85999ceff53a03efffc499e
alado <alado@DESKTOP-NIECQLT>
2021-05-07 01:42:05 +0200

    Fallan 2 Merge y Countin

--- Algoritmia/src/Algorithms/CountingSort.java
+++ Algoritmia/src/Algorithms/CountingSort.java
@@ -1,34 +1,49 @@
 package Algorithms;
 
 public class CountingSort {
-	
-	public static void main(String[] args) { 
+
+	public static void main(String[] args) {
 		System.out.println("Disordered List:");
-		
-		//hay que usar recursividad
-		int [] lista = new int[10]; //the value between the square brackets defines the length of the List
-		int numero = 1000;
-		
-		for(int x = 0; x<lista.length ;x++) {
-			int numRandom = (int) (Math.random()*numero);
-			lista[x]= numRandom;		
-			System.out.print(lista[x]+" ");
+
+		// hay que usar recursividad
+		int numero = 10;
+		int[] lista = new int[numero]; // the value between the square brackets defines the length of the List
+		//int maximo = 0;
+		for (int x = 0; x < lista.length; x++) {
+			int numRandom = (int) (Math.random() * numero);
+			lista[x] = numRandom;
+			//if (lista[x]>maximo){
+				//maximo = lista[x];
+			//}
+			System.out.print(lista[x] + " ");
+			
 		}
 		System.out.println("\n");
-		System.out.println("Ordered list:");}
+		System.out.println("Ordered list:");
 		
+		int [] listaB = new int[numero];
 		
-		 static void countingSort(int[] lista,int[] listaB,int mayor){
-		    	int[] C= new int[mayor];
-		    	for(int i=0;i<mayor;i++) {
-		    		C[i]=0;}
-		    	for(int j=1;j<lista.length;j++) {
-		    		C[lista[j]]++;}
-		    	for(int i=1;i<mayor;i++) {
-		    		C[i]=C[i]+C[i-1];}
-		    	for(int j=lista.length-1;j>0;j--){
-		    		listaB[C[lista[j]]]=lista[j];
-		    		C[lista[j]]--;
-		    	}
-	}	
+		countingSort(lista, listaB, numero);		
+		for (int x = 0; x < listaB.length; x++) {
+			System.out.print(listaB[x] + " ");	
+		}
+		
+	}
+
+	public static void countingSort(int[] lista, int[] listaB, int mayor) {
+		int[] C = new int[mayor];
+		for (int i = 0; i < mayor; i++) {
+			C[i] = 0;
+		}
+		for (int j = 1; j < lista.length; j++) {
+			C[lista[j]]++;
+		}
+		for (int i = 1; i <mayor; i++) {
+			C[i] = C[i] + C[i - 1];
+		}
+		for (int j = lista.length - 1; j > 0; j--) {
+			listaB[C[lista[j]]] = lista[j];
+			C[lista[j]]--;
+		}
+	}
 }
--- Algoritmia/src/Algorithms/MergeSort.java
+++ Algoritmia/src/Algorithms/MergeSort.java
@@ -17,30 +17,37 @@ public class MergeSort {
 		System.out.println("\n");
 	    System.out.println("Ordered list:");
 	    
-	    
+	    MergeSort2(lista, 0, lista.length);
+	    for(int x = 0; x<lista.length ;x++) { 		//Fills the array with random numbers between 1 to 100		
+			System.out.print(lista[x]+" ");	
+			}	    
 		}
 		
 		
-		public static void MergeSort2 (int[] lista,int p, int r) {
-			if (p < r);{
-				int q = (p + r)/2;
+		public static void MergeSort2 (int[] lista, int p, int r) {
+			if (p < r){
+				int q = ((p + r)/2);
 				MergeSort2 (lista,p,q);
-				MergeSort2 (lista,p+1,r);
-				MergeSort.Merge(lista,p,q,r);}
+				MergeSort2 (lista,q+1,r);
+				MergeSort.Merge(lista,p,q,r);
+			}
 			}
 		
 		
 		public static void Merge (int[] lista, int p, int q, int r) {
 			int n1=q-p+1;
 	    	int n2=r-q;
-	    	int[] L=new int[n1+1];
-	    	int[] R=new int[n2+1];
-	    	for(int i=0;i<n1;i++) {
-	    		L[i]=lista[p+i];}
-	    	for(int j=0;j<n2;j++) {
-	    		R[j]=lista[q+j+1];}
+	    	int L []= new int[n1+1];
+	    	int R []= new int[n2+1];
+	    	for(int i=0;i<=n1;i++) {
+	    		L[i]=lista[p+i];
+	    		}
 	    	L[n1]= Integer.MAX_VALUE;
-	    	R[n2]= Integer.MAX_VALUE;
+	    	for(int j=0;j<=n2;j++) {
+	    		R[j]=lista[q+j+1];
+	    		}
+	    	R[n2+1]= Integer.MAX_VALUE;
+	    	
 	    	int i=0;
 	    	int j=0;
 	    	for(int k=p;k<=r;k++){
